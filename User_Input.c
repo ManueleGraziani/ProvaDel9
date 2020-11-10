c24cbc43b553e9ee9b4a23bbf82e5806f8f22c20
ManueleGraziani <manuele.rosso@live.it>
2020-11-10 19:18:34 +0100

    Correzione Input

--- User_Input.c
+++ User_Input.c
@@ -1,66 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <math.h>
 #include "ProvaDel9.h"
-
 
 int  User_Input(int Size){
 
-        int  UserInput = 0  , ExitValue = -1; 
-        char Buffer[10] = "\0" , *endptr  = NULL;
+	int  UserInput = 0  , ExitValue = 0, InputSize = 0; 
 	
-        do{
-				
-		if( Size == 4)
-			scanf("%5s",Buffer); /*Legge solamente i primi cinque caratteri inseriti dall'utente impendendo 
-					       overflow del Buffer (il primo carattere è riservato per il segno)*/
-		else
-			scanf("%9s",Buffer);
-
-		for(int i=0;i<10;i++)
-		printf("Buffer[%d] %c : %d \n",i, Buffer[i],Buffer[i]);
-
-                if(getchar() == '\n'){
-
-                        errno = 0;
-
-               		UserInput = (int) strtol (Buffer,&endptr, 10);
-
-			if(UserInput > 9999 && Size == 4 || UserInput > 99999999 && Size == 8){
-				printf("Errore, valore inserito superiore alla dimensione massima consentitai di: %d cifre. Riprovare!!\n",Size);
-				ExitValue = -1;
-			}else
-				ExitValue = 0;
-
-                        //Controllo sui possibili errori
+	do{
+		errno = 0;
+		scanf("%d", &UserInput);
 		
-                        if(errno != 0){
-                                printf("Error: strtol %d",errno);
-                                exit(EXIT_FAILURE);
-                        }
+		ExitValue = 0;
 
+		if(errno != 0){
 
-                        if(*endptr != '\0'){
-                                printf("Per favore inserire valori appartenenti all'insieme N\n\n");
-                                ExitValue = -1;
-                        }
+			perror("scanf");
+			exit(EXIT_FAILURE);
+		}
+	       
+		if(getchar() != '\n'){
 
+			printf("Per favore inserire valori appartenenti all'insieme N\n\n");
+			while(getchar() != '\n');
+			ExitValue = -1;
+		}
 
+		InputSize = 1 + log10(fabs(UserInput));
 
-                }else{
+		if(InputSize > Size){
+			printf("Errore, valore inserito superiore alla dimensione massima consentitai di: %d cifre. Riprovare!!\n",Size);
+			ExitValue = -1;
+		}
 
-                        printf("Errore, valore inserito superiore alla dimensione massima consentitai di: %d cifre. Riprovare!!\n",Size);
-                        while(getchar() != '\n'); /*Nell’eventualità di un input di dimensioni maggiori di due caratteri, i caratteri 
-                                                   in eccesso verranno cancellati fino al raggiungimento dell’carattere terminazione*/
+	}while(ExitValue == -1);
 
-                        ExitValue = -1;
-
-                }
-
-
-
-
-        }while(ExitValue == -1);
-
-        return UserInput;
+	return UserInput;
 }
+
+
+
